3a51fbbe3ef10f4c973ef6b39b0c7604ef1aa234
PJ Muller <pjmuller@gmail.com>
2026-01-06 06:07:27 +0100

    deploy to github pages

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.GITHUB_ACTIONS ? '/photobook/' : '/',
   server: {
     port: 3000
   }
